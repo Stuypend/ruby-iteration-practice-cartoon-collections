56094dc242a3573f573567ab38b8ee9400851851
stuypend <andre@stuypend.com>
2021-02-22 17:18:37 -0500

    commit tree 5a329a27d587c76966d53d4efc3abca38e1aa98f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  list = ""
+
+  dwarves.each_with_index do
+    |dwarf, idx|
+    print "> #{idx+1}. #{dwarf} "
+  end
+  #puts list
+  binding.pry
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do
+    |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if (cheeses.include?("cheddar"))
+    return "cheddar"
+    end
+  if (cheeses.include?("gouda"))
+     return "gouda"
+     end
+  if  (cheeses.include?("camembert"))
+    return "camembert"
+    end
+
+
+  return nil
 end
+
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
